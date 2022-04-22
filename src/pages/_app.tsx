b9090655383960b28f89e77e3e5871760f7316f5
RuiOkazaki <okazaki200226@gmail.com>
2022-04-22 16:02:48 +0900

    refactor: functonからarrow functionに変更した

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,12 +3,12 @@ import { ChakraProvider } from '@chakra-ui/react';
 import theme from '../theme';
 import { AppProps } from 'next/app';
 
-function MyApp({ Component, pageProps }: AppProps) {
+const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <ChakraProvider resetCSS theme={theme}>
       <Component {...pageProps} />
     </ChakraProvider>
   );
-}
+};
 
 export default MyApp;
